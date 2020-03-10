2792bf7bd593c79e5258828149c760068a4379ad
felunka <felixvontiedemann@gmail.com>
2020-03-10 12:51:00 +0100

    Add user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'user/show'
-  get 'user/create'
-  get 'user/index'
-  get 'user/update'
-  get 'user/delete'
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_203409) do
+ActiveRecord::Schema.define(version: 2020_03_10_114640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "password_digest"
     t.string "firstname"
     t.string "lastname"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

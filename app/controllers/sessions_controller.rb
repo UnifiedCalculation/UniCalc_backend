f5654d6815c0b641da3f45e10bc90ac175492c0e
felunka <felixvontiedemann@gmail.com>
2020-05-16 11:30:39 +0200

    Add login error feedback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
+      @error = "Email or password is invalid"
       render "new"
     end
   end

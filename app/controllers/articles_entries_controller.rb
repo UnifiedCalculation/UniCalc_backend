71044cc8da854cb749e465406cb77c4a6876b9db
felunka <felixvontiedemann@gmail.com>
2020-05-17 20:52:18 +0200

    Update articleEntry index and add update

--- app/controllers/articles_entries_controller.rb
+++ app/controllers/articles_entries_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesEntriesController < ApiController
   def index
-    @articles_entries = ArticlesEntry.where entry_id: params[:entry_id]
+    @articles_entries = ArticlesEntry.select(:article_id, :entry_id, :name, :amount, :discount, :price, :description, :unit).joins(:article).where entry_id: params[:entry_id]
 
     render json: @articles_entries
   end
@@ -9,5 +9,11 @@ class ArticlesEntriesController < ApiController
     @articles_entry = ArticlesEntry.create! params.permit(:article_id, :entry_id, :description, :amount, :discount)
 
     render json: @articles_entry
+  end
+
+  def update
+    article_entry = ArticlesEntry.find_by entry_id: params[:entry_id], article_id: params[:id]
+
+    article_entry.update params.require(:article_entry).permit(:amount, :description, :discount)
   end
 end

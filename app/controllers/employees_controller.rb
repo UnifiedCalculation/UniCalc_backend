d2d4b65cb950396f98c4a9c2f7deb1099d055d25
felunka <felixvontiedemann@gmail.com>
2020-05-18 18:49:18 +0200

    Add Employee update and create

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,4 +10,33 @@ class EmployeesController < ApiController
 
     render json: employee
   end
+
+  def create
+    user = User.new params.permit(:email, :firstname, :lastname)
+    user.password = user.password_confirmation = (0...16).map { ('a'..'z').to_a[rand(26)] }.join
+    user.save!
+
+    params[:roles].each do |role_str|
+      user.roles << Role.find_by(name: role_str)
+    end
+
+    employee = Employee.create! user: user, company: current_user.company
+
+    render json: employee
+  end
+
+  def update
+    employee = Employee.find params[:id]
+    user = employee.user
+
+    user.update! params.permit(:firstname, :lastname, :email)
+
+    user.roles.destroy_all
+
+    params[:roles].each do |role_str|
+      user.roles << Role.find_by(name: role_str)
+    end
+
+    render json: employee
+  end
 end

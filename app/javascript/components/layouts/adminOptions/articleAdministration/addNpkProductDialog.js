6084b26b83996063533e226891c612713366c094
RicardoMonteiroSimoes <RicardoMonteiroSimoes@users.noreply.github.com>
2020-05-19 20:07:09 +0000

    Update file(s) "/src/components/" from "UnifiedCalculation/UniCalc_Frontend"

--- app/javascript/components/layouts/adminOptions/articleAdministration/addNpkProductDialog.js
+++ app/javascript/components/layouts/adminOptions/articleAdministration/addNpkProductDialog.js
@@ -80,8 +80,12 @@ const AddNpkProductDialog = ({setErrorMessage, onCancel, onSubmit, show, setProd
 
   const parseArticleData = (articleData) => {
     articleData.name = npks.find(entry => entry.number == articleData.npk.split(" ")[0].split(".")[0]).name;
-    articleData.npk_id = npks.find(entry => entry.number == articleData.npk.split(" ")[0].split(".")[1]).id;
+    console.log(articleData);
+    console.log(npks.find(entry => entry.number == articleData.npk.split(" ")[0].split(".")[1]));
+    console.log(npks.find(entry => entry.number == articleData.npk.split(" ")[0].split(".")[1]).npk_id);
     articleData.number = articleData.npk_id + "." + articleData.number;
+    articleData.npk_id = npks.find(entry => entry.number == articleData.npk.split(" ")[0].split(".")[1]).npk_id;
+    console.log(articleData)
     saveNewArticle(articleData);
   }
 
